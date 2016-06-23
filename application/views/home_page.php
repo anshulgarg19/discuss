2312ca24795afbb407d95e489d91d6b222c2f777
anshulgarg19 <garg_anshul94@ymail.com>
2016-06-23 14:59:20 +0530

    register/login validations

--- application/views/home_page.php
+++ application/views/home_page.php
@@ -10,6 +10,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<!-- login form -->
 	<form>
 		<label for="email">E-mail </label><input type="text" name="email" id="login_email"><br/>
+		<div id="error-login_email"></div><br/>
 		<label for="password">Password </label><input type="password" name="password" id="login_password"><br/>
 		<button type="button" id="login_submit">Submit</button>
 	</form>
@@ -27,9 +28,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		<label for="fname">First Name </label><input type="text" name="fname" id="first_name"><br/>
 		<label for="lname">Last Name </label><input type="text" name="lname" id="last_name"><br/>
 		<label for="pnum">Phone Num </label><input type="text" name="pnum" id="phone_num"><br/>
+		<div id="error-pnum" class="error-div"></div><br/>
 		<label for="email">Email </label><input type="text" name="email" id="register_email"><br/>
+		<div id="error-email" class="error-div"></div><br/>
 		<label for="password">Password </label><input type="password" name="password" id="register_password"><br/>
-		<label for="confirm_pw">Confirm Password</label><input type="password" name="confirm_pw" id="confirm_password"><br/>
+		<div id="error-password" class="error-div"></div><br/>
+		<label for="confirm_pw">Confirm Password</label><input type="password" name="confirm_pw" id="confirm_password">
+		<br/>
+		<div id="error-confirm_password" class="error-div"></div><br/>
 		<button type="button" id="register_submit">Register</button>
 	</form>
 
--- static/js/homepage.js
+++ static/js/homepage.js
@@ -1,48 +1,146 @@
-$('#login_submit').click(function(event) {
 
-	var data = {
-		email: $('#login_email').val(),
-		password: $('#login_password').val()
-	};
+$(document).ready(function(){
+	//onClick function for login submit
+	$('#login_submit').click(function(event) {
 
-	$.ajax({
-		url: '/index.php/homepage/login',
-		type: 'POST',
-		data: data
-	})
-	.success(function(response) {
-		console.log(response);
-		console.log("success");
-		$('#result').html(response);
-	});
-			
-});
+		var invalid = false;
 
-//onClick function for register submit
-$('#register_submit').click(function(event){
+		var login_email = $('#login_email').val();
 
-	var data = {
-		fname : $('#first_name').val(),
-		lname : $('#last_name').val(),
-		phone_num : $('#phone_num').val(),
-		email : $('#register_email').val(),
-		password : $('#register_password').val(),
-		confirm_password : $('#confirm_password').val()
-	}
-
-	$.ajax({
-		url : 'index.php/homepage/register',
-		type: 'POST',
-		data : data,
-		success: function(response){
-			console.log(response);
-			console.log("register success");
-			$('#register_response').html(response);
-		},
-		error: function(response){
-			console.log(response);
-			console.log("register failure");
-			$('#register_response').html(response.responseText);
+		//validate login email
+		if( !validate_email(login_email) )
+		{
+			$("#error-login_email").css("color","red");
+			$("#error-login_email").html("Invalid Email format");
+			invalid = true;
 		}
+
+		if( invalid )
+			return;
+
+		var data = {
+			email: login_email,
+			password: $('#login_password').val()
+		};
+
+		$.ajax({
+			url: '/index.php/homepage/login',
+			type: 'POST',
+			data: data
+		})
+		.success(function(response) {
+			console.log(response);
+			console.log("success");
+			$('#result').html(response);
+		});
+				
 	});
+
+	//onClick function for register submit
+	$('#register_submit').click(function(event){
+
+		//values stored in variables
+		var first_name = $('#first_name').val();
+		var last_name = $('#last_name').val();
+		var pnum = $('#phone_num').val();
+		var register_email = $('#register_email').val();
+		var register_password = $('#register_password').val();
+		var confirm_passwd = $('#confirm_password').val();
+
+		var invalid = false;
+
+		// integer phone num validation
+		if( !validate_phonenumber(pnum) )
+		{
+			$('#error-pnum').css("color","red");
+			$('#error-pnum').html("Phone Number can only be 10 digits");
+			invalid = true;
+		}
+
+		//validate email
+		if( !validate_email(register_email) )
+		{
+			$("#error-email").css("color","red");
+			$("#error-email").html("Invalid email format");
+			invalid = true;
+		}
+
+		//validate password
+		if( !validate_password( register_password) )
+		{
+			$("#error-password").css("color","red");
+			$("#error-password").html("Invalid password.Length should be between 6 to 20 characters");
+			invalid = true;
+		}
+
+		//validate confirm password
+		if( confirm_passwd != register_password )
+		{
+			$("#error-confirm_password").css("color","red");
+			$("#error-confirm_password").html("Password doesnt match");
+			invalid = true;
+		}
+
+		if( invalid )
+			return;
+
+		var data = {
+			fname : first_name,
+			lname : last_name,
+			phone_num : pnum,
+			email : register_email,
+			password : register_password,
+			confirm_password : confirm_passwd
+		}
+
+		$.ajax({
+			url : 'index.php/homepage/register',
+			type: 'POST',
+			data : data,
+			success: function(response){
+				console.log(response);
+				console.log("register success");
+				$('#register_response').html(response);
+			},
+			error: function(response){
+				console.log(response);
+				console.log("register failure");
+				$('#register_response').html(response.responseText);
+			}
+		});
+	});
+
+	//function to validate phone number
+	function validate_phonenumber(inputtxt)  
+	{  
+	  	var phoneno = /^\d{10}$/;  
+	  	if( inputtxt.match(phoneno) )  
+		{  
+	      	return true;  
+	    }  
+	    else  
+	    {  
+	        return false;  
+	    }  
+	}  
+
+	//function to validate email
+	function validate_email( inputtxt ) {
+	    var atpos = inputtxt.indexOf("@");
+	    var dotpos = inputtxt.lastIndexOf(".");
+	    if (atpos<1 || dotpos<atpos+2 || dotpos+2>=inputtxt.length) {
+	        return false;
+	    }
+	    return true;
+    }
+
+    //function to validate password
+    function validate_password(inputtxt){
+    	if( inputtxt.length < 6 || inputtxt.length > 20 ){
+    		console.log("pwd check");
+    		return false;
+    	}
+    	return true;
+    }
+
 });
