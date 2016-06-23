bec79159ccd1ff6a707480542d748ff360b870af
avishkar <avishkar.gupta@99acres.com>
2016-06-23 12:31:15 +0530

    Added user model

---
+++ application/models/User_model.php
@@ -0,0 +1,71 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+	// Model for the user information
+	class User_Model extends CI_Model {
+		
+		// Details of user		
+		private $id;
+		private $firstname;
+		private $lastname;
+		private $phone;
+		private $email;
+
+		// invoke the parent class constructor
+		function __construct() {
+			parent::__construct();
+		}
+
+
+	    // Getters
+	    public function getId()
+	    {
+	        return $this->id;
+	    }
+
+	    public function _setId($id)
+	    {
+	        $this->id = $id;
+	    }
+
+	    public function getFirstname()
+	    {
+	        return $this->firstname;
+	    }
+
+	    public function _setFirstname($firstname)
+	    {
+	        $this->firstname = $firstname;
+	    }
+
+	    public function getLastname()
+	    {
+	        return $this->lastname;
+	    }
+
+	    public function _setLastname($lastname)
+	    {
+	        $this->lastname = $lastname;
+	    }
+
+	    public function getPhone()
+	    {
+	        return $this->phone;
+	    }
+
+	    public function _setPhone($phone)
+	    {
+	        $this->phone = $phone;
+	    }
+
+	    public function getEmail()
+	    {
+	        return $this->email;
+	    }
+
+	    public function _setEmail($email)
+	    {
+	        $this->email = $email;
+	    }
+}
+?>
