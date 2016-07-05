2c79f7642ab6ace5be4ed7e2617c4d96730be4a2
rootavish <rootavish@gmail.com>
2016-07-05 16:07:58 +0530

    Fixed modal link in navbar

--- application/views/header.php
+++ application/views/header.php
@@ -62,7 +62,7 @@
 	      </form>
 	      <ul class="nav navbar-nav navbar-right">
 	        <li><a href="/index.php/userprofile/showprofile" target="_blank"><?php echo $_SESSION['firstname'];?></a></li>
-			<li><a data-toggle="modal" data-target="#question-modal">Ask Question</a></li>
+			<li><a href="" data-toggle="modal" data-target="#question-modal">Ask Question</a></li>
 			<li><a href="/index.php/userhome/logout">Logout</a></li>
 	      </ul>
 	    </div><!-- /.navbar-collapse -->
