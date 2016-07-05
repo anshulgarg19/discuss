da055f26e1ee42a14e98c0364f3aa5d5950518eb
rootavish <rootavish@gmail.com>
2016-07-05 13:41:18 +0530

    changing model import names everywhere

--- application/libraries/Answerlib.php
+++ application/libraries/Answerlib.php
@@ -8,29 +8,28 @@ class Answerlib{
 	public function __construct(){
 		
 		$this->_ci =& get_instance();
-		$this->_ci->load->model("Answer_model");
-		$this->answer_object = new Answer_model();
+		$this->_ci->load->model("answer_model");
 		$this->_ci->load->library('session');
 	}
 
 	public function postAnswer($data){
 		//var_dump($data['question']);
-		//$this->answer_object->addAnswerToQuestion($data['question'],56,$data['answer_content']);
+		//$this->_ci->answer_model->addAnswerToQuestion($data['question'],56,$data['answer_content']);
 		$data['user_id'] = $_SESSION['user_id'];		//change to $_SESSION['id']
-		$this->answer_object->addAnswerToQuestion($data);
+		$this->_ci->answer_model->addAnswerToQuestion($data);
 	}
 
 	public function showAnswers($question_id,$offset, $limit){
-		return $this->answer_object->getAnswersToQuestion($question_id,$offset,$limit);
+		return $this->_ci->answer_model->getAnswersToQuestion($question_id,$offset,$limit);
 	}
 
 	public function get_user_for_answer($question_id)
 	{
-		return $this->answer_object->getUserForAnswer($question_id);
+		return $this->_ci->answer_model->getUserForAnswer($question_id);
 	}
 
 	public function get_answers_for_user($user,$offset,$limit){
-		return $this->answer_object->getAnswersForUser($user,$offset,$limit);
+		return $this->_ci->answer_model->getAnswersForUser($user,$offset,$limit);
 	}
 }
 ?>
